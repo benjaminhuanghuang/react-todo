9428aea27b19a52fba61945a90c8ee73eb105938
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-04-25 22:41:37 -0700

    update

---
+++ src/components/todo/TodoItem.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const TodoItem =(props)=> {
+    return (
+            <li>
+                <input type="checkbox" defaultChecked={props.isComplete}/>{props.name}
+            </li>)
+}
